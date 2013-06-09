246159b60ca4bf3cd0a3a7348f6a1a0bc0a4fb60
Genki Sugimoto <cfhoyuk.reccos.nelg@gmail.com>
2013-06-09 12:17:36 +0900

    Complete minimum tests for :Annotate command

--- spec/plugin/annotation_spec.rb
+++ spec/plugin/annotation_spec.rb
@@ -1,16 +1,36 @@
 require 'spec_helper'
 
-describe 'Annotation' do
+describe ':Annotate' do
   let(:annotation_dir) { "#{Dir.pwd}/.annotation" }
+  let(:annotation_filetype) { 'markdown' }
+  let(:file) { Tempfile.new('annotation') }
+  let(:filename) { file.path }
+  let(:contents_dir) { filename.gsub('/', '%') }
 
   before(:each) do
     # Set plugin options
     vim.let 'g:annotator#annotation#directory', annotation_dir
+    vim.let 'g:annotator#annotation#filetype', annotation_filetype
+
+    # Create Sandbox
+    vim.edit!(filename)
+    vim.insert('Hello, World.')
+
+    # Invoke :Annotate
+    vim.command('Annotate')
   end
 
-  describe '#create_directory' do
-    it 'creates directory with g:annotator#annotation#directory option' do
-      File.directory?(annotation_dir).should be_true
-    end
+  it 'creates contents directory with buffer full path (with "/" substituted by "%")' do
+    File.directory?(File.join(annotation_dir, contents_dir)).should be_true
+  end
+
+  it 'opens an annotation file corresponding to current buffer and line no.' do
+    buffer = vim.echo 'expand("%:p")'
+    buffer.should eq File.join(annotation_dir, contents_dir, '1')
+  end
+
+  it 'sets annotation buffer filetype with g#annotator#annotation#filetype' do
+    filetype = vim.echo '&filetype'
+    filetype.should eq annotation_filetype
   end
 end
