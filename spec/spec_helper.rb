f098360a764a8550f654a9b8483e8f1ab1a90e47
Genki Sugimoto <cfhoyuk.reccos.nelg@gmail.com>
2013-06-09 10:01:19 +0900

    Define let method for vim instance to set variables in vim

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,11 +9,16 @@ Vimrunner::RSpec.configure do |config|
   # Decide how to start a Vim instance. In this block, an instance should be
   # spawned and set up with anything project-specific.
   config.start_vim do
-    vim = Vimrunner.start
+    vim = Vimrunner.start_gvim
 
     # Setup your plugin in the Vim instance
     plugin_path = File.expand_path('../..', __FILE__)
     vim.add_plugin(plugin_path, 'plugin/vim-annotator.vim')
+
+    def vim.let(var, val)
+      val = "'#{val}'" if val.is_a? String
+      command "let #{var}=#{val}"
+    end
 
     # The returned value is the Client available in the tests.
     vim
