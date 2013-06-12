5e985b4d44574ec81768b349987f57ff6adf1207
Genki Sugimoto <cfhoyuk.reccos.nelg@gmail.com>
2013-06-12 11:51:51 +0900

    Add supportive methods to access annotation contents

--- spec/support/vim.rb
+++ spec/support/vim.rb
@@ -8,5 +8,13 @@ module Support
     def set_file_contents_with_line_numbers(num)
       set_file_contents((1..num).to_a.join("\n"))
     end
+
+    def annotation_filename_of_line(line)
+      File.join(@annotation_dir, @contents_dir, line.to_s)
+    end
+
+    def get_annotation_contents_of_line(line)
+      File.read(annotation_filename_of_line(line))
+    end
   end
 end
